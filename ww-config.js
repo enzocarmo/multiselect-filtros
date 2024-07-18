6f01965136aba47677491041741139042b919cf3
Enzo Carmo <enzocarmo64@gmail.com>
2024-07-18 19:55:20 -0300

    Start

---
+++ ww-config.js
@@ -0,0 +1,32 @@
+export default {
+  editor: {
+    label: {
+      en: "MultiSelect Lojas",
+    },
+  },
+  properties: {
+    data: {
+      label: {
+        en: "Data",
+      },
+      type: "Array",
+      defaultValue: [],
+      bindable: true,
+      section: "Settings"
+    },
+    placeholder: {
+      label: {
+        en: "Placeholder"
+      },
+      type: "Text",
+      section: "Settings"
+    },
+    label: {
+      label: {
+        en: "Label"
+      },
+      type: "Text",
+      section: "Settings"
+    }
+  },
+};
